100582e18f5cbf6dbd12161b0200f55e244800c5
Muhideen Mujeeb Adeoye <mujeeb.muhideen@gmail.com>
2026-08-28 06:25:00 +0100

    🎨 style(go): format headgatectl test

--- go/headgatectl/main_test.go
+++ go/headgatectl/main_test.go
@@ -8,6 +8,7 @@ import (
 )
 
 type roundTripFunc func(*http.Request) (*http.Response, error)
+
 func (f roundTripFunc) RoundTrip(r *http.Request) (*http.Response, error) { return f(r) }
 
 func TestClientUsesBoundedControlAPIAndBearerAuthentication(t *testing.T) {

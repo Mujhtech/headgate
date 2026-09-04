6fb3becf8f88be111da5e743cef0792c141e2266
Muhideen Mujeeb Adeoye <mujeeb.muhideen@gmail.com>
2026-09-04 11:36:28 +0100

    🏗️ chore(ui): rebuild embedded console assets

---
+++ ui/dist/assets/_console-DHl47o95.js
@@ -0,0 +1,1 @@
+import{t as e}from"./console-Cm9CRB2q.js";var t=e;export{t as component};
---
+++ ui/dist/assets/_console.workflows-2LWgVBBk.js
@@ -0,0 +1,1 @@
+import{s as e}from"./utils-VRcxtyuh.js";import{s as t}from"./console-Cm9CRB2q.js";import{n}from"./workflows-Bdi9X82w.js";var r=e();function i(){return(0,r.jsx)(n,{...t()})}export{i as component};
